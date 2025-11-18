60fd7d649ed0abe4e358c9dd9d9f03e8270df3ec
NuriaRodriguezPardo <n.r.pardo04@gmail.com>
2025-11-18 15:26:54 +0100

    Añadido multiples ficheros en leeLis

--- ramses/util.py
+++ ramses/util.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 def leeList (fitLis): 
     # with apertura [as objeto]
     # No siempre apertura saca un objeto. Utilizaremos pytorch 
@@ -8,4 +10,31 @@ def leeList (fitLis):
         for linea in fpLis: 
             lista.append(linea.strip())
     return lista
- 
+
+
+def pathName(dir, name: str, ext):
+    '''
+        Te retorna una cadena de Strings con el nombre del path
+    '''
+    # se puede hacer también con 
+    # return dir + '/' + name + '.' + ext
+    # f"{dir}/{name}{ext}"
+
+    # Libreria para generación de path
+    # https://docs.python.org/3/library/pathlib.html
+
+    if ext[0] != '.': ext = f".{ext}"
+    if name.startswith(dir): name = 
+    return Path(dir).joinpath(name).with_suffix(ext)
+
+
+def leeLis(*ficLis):
+    """
+        Lee el contenido de uno o más ficheros de texto, devolviendo las palabras en ellos
+        contenidas en la forma de lista de cadenas de texto.
+    """
+    lista = []
+    for fichero in ficLis:
+        with open(fichero, 'rt') as fpLis:
+            lista += [pal for linea in fpLis for pal in linea.split()]
+    return lista
