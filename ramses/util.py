faacd7975e6197a7896b588a5e1fc11fd4d55f01
Eduard Peñas <eduard.penas@estudiantat.upc.edu>
2025-11-18 15:31:57 +0100

    Lo que tuvieras

--- ramses/util.py
+++ ramses/util.py
@@ -1,14 +1,14 @@
 from pathlib import Path 
 
-def leeList (fitLis): 
-    # with apertura [as objeto]
-    # No siempre apertura saca un objeto. Utilizaremos pytorch 
-    # with torch_nograd() -> no devuelve nigun objeto pero va muy rápido
-    # with apertura as objeto: -> si nos devuelve un objeto
-    with open(fitLis, "rt") as fpLis: 
-        lista = []
-        for linea in fpLis: 
-            lista.append(linea.strip())
+def leeLis(*ficLis):
+    """
+    Lee el contenido de uno o más ficheros de texto, devolviendo las palabras en ellos
+    contenidas en la forma de lista de cadenas de texto.
+    """
+    lista = []
+    for fic in ficLis:
+        with open(fic, 'rt') as fpLis:
+         lista += [pal for linea in fpLis for pal in linea.split()]
     return lista
 
 
@@ -18,3 +18,11 @@ def pathName(dir, name: str, ext):
     return Path(dir).joinpath(name).with_suffix(ext)
 
 
+def chkPathName(pathName):
+    """
+    Crea, en el caso de que no exista previamente, el directorio del fichero
+    'pathName'.
+    ···
+    ↪→
+    """
+    Path(pathName).parent.mkdir(parents=True, exist_ok=True)
